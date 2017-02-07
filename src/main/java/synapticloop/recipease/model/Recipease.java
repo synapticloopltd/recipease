4bc8441e25bd64fba660a5094582c309efb47a2b
synapticloop <contact@synapticloop.com>
2017-02-07 13:56:32 +0000

    updated titles, added more recipes

--- src/main/java/synapticloop/recipease/model/Recipease.java
+++ src/main/java/synapticloop/recipease/model/Recipease.java
@@ -16,13 +16,19 @@ public class Recipease {
 	@JsonProperty("title") private String title;
 	@JsonProperty("image") private String image;
 	@JsonProperty("subTitle") private String subTitle;
-	@JsonProperty("tocTitle") private String tocTitle;
+	@JsonProperty("tocTitle") private String tocTitle = "Table of Contents";
+	@JsonProperty("ingredientsTitle") private String ingredientsTitle = "Ingredients";
+	@JsonProperty("notesTitle") private String notesTitle = "Notes";
+	@JsonProperty("directionsTitle") private String directionsTitle = "Directions";
 	@JsonProperty("sections") private List<Section> sections;
 
 	public String getTitle() { return this.title; }
 	public String getImage() { return this.image; }
 	public String getSubTitle() { return this.subTitle; }
 	public String getTocTitle() { return this.tocTitle; }
+	public String getIngredientsTitle() { return this.ingredientsTitle; }
+	public String getNotesTitle() { return this.notesTitle; }
+	public String getDirectionsTitle() { return this.directionsTitle; }
 	public List<Section> getSections() { return this.sections; }
 
 	@JsonAnySetter
